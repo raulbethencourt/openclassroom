76f627436f69d311183c5d8323811a52a69e84d9
Raul <raul.bethencourt.pro@gmail.com>
2021-01-19 11:39:40 +0100

    :sparkles: coming back

---
+++ Write JavaScript for the Web/test.js
@@ -0,0 +1,10 @@
+// DOM ELEMENT REFERENCES
+const hobbiesResult = document.getElementById('hobbies-result');
+const transportResult = document.getElementById('transport-result');
+const musicResult = document.getElementById('music-result');
+const sports = document.getElementById('sports-checkbox');
+
+sports.addEventListener("change", (evt) => {
+
+  console.log('test');
+});
